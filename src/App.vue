d7d8d047cf25c9b406c52213ac92dab99aefe5cf
Benjamin <benjamin@trevald.com>
2020-07-17 20:53:04 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <div class="container">
+        <A />
+        <A state="hover" />
+        <A state="focus" />
+        <A state="active" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import A from "./components/tags/A";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    A
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/tags/A.vue
@@ -0,0 +1,52 @@
+<template>
+  <p>
+    <a
+      href="#"
+      :class="className"
+      @click.prevent="doNothing"
+    >This is {{text}} state </a>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "A",
+
+  props: {
+    state: String
+  },
+
+  computed: {
+    className() {
+      switch (this.state) {
+        case "focus":
+          return "is-focus";
+        case "active":
+          return "is-active";
+        case "hover":
+          return "is-hover";
+        default:
+          return "";
+      }
+    },
+    text() {
+      switch (this.state) {
+        case "focus":
+          return "text link in focus";
+        case "active":
+          return "text link in active";
+        case "hover":
+          return "text link in hover";
+        default:
+          return "text link in idle";
+      }
+    }
+  },
+
+  methods: {
+    doNothing() {
+      return;
+    }
+  }
+};
+</script>
